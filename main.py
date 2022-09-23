8c64edc879194e7530c599dd3a2add1475f3ffd7
Huy Ly <huylg2109@gmail.com>
2022-09-23 19:26:10 +0700

    create the database and build RESTful for page

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+import datetime
 
-posts = requests.get('https://api.npoint.io/f93148253becf328fcf9').json()
+
+# Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def home():
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+# WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+@app.route('/')
+def get_all_posts():
+    posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = BlogPost.query.get(index)
+    return render_template("post.html", post=requested_post)
 
 
 @app.route("/about")
@@ -21,14 +66,53 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/post/<int:index>")
-def get_post(index):
-    requested_post = None
-    for post in posts:
-        if int(post['id']) == index:
-            requested_post = post
-            break
-    return render_template("post.html", post=requested_post)
+@app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        author=post.author,
+        img_url=post.img_url,
+        body=post.body
+    )
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.subtitle = form.subtitle.data
+        post.author = form.author.data
+        post.img_url = form.img_url.data
+        post.body = form.body.data
+        db.session.commit()
+        return redirect(url_for('show_post', index=post.id))
+    return render_template('make-post.html', form=form, is_edit=True)
+
+
+@app.route("/new-post", methods=['GET', 'POST'])
+def create_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        today = datetime.datetime.now()
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            date=today.strftime("%B %d, %Y"),
+            img_url=form.img_url.data,
+            body=form.body.data
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template('make-post.html', form=form)
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    post = BlogPost.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
